0202d3576b355ab98ac1045c9f8488968fcf6976
Xinzzz <296232375@qq.com>
2025-10-03 10:49:22 +0800

    mouse icon

--- Entities/Scripts/bullet_1.gd
+++ Entities/Scripts/bullet_1.gd
@@ -1,5 +1,14 @@
 extends Area2D
 
+@export var speed = 50
+var direction = Vector2.RIGHT
+
+func _process(delta: float) -> void:
+	translate(direction * speed * delta)
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	queue_free()
+
+
+func _on_visible_screen_exited() -> void:
+	queue_free()
--- Entities/Scripts/player.gd
+++ Entities/Scripts/player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
 @export var speed: int
+enum player_states {MOVE, DEAD}
 
 var pos
 var rot
+var is_dead = false
+var current_state = player_states.MOVE
 
+
+@onready var bullet_scene = preload("res://Entities/Scenes/Bullets/bullet_1.tscn")
 @onready var gun = $Gun_handler
 @onready var gun_sprite = $Gun_handler/Gun_sprite
 @onready var bullet_point = $Gun_handler/Bullet_point
@@ -15,9 +20,17 @@ func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
-	movement(delta)	
 	target_mouse()
-
+	
+	if player_data.health <= 0:
+		current_state = player_states.DEAD
+	
+	match current_state:
+		player_states.MOVE:
+			movement(delta)	
+		player_states.DEAD:
+			dead()
+			
 func movement(delta):
 	input_movement = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	anim_play()
@@ -29,6 +42,17 @@ func movement(delta):
 	
 	move_and_slide()
 
+func dead():
+	is_dead = true
+	velocity = Vector2.ZERO
+	gun.visible = false
+	$Anim.play("Dead")
+	await get_tree().create_timer(2).timeout
+	if get_tree():
+		get_tree().reload_current_scene()
+		player_data.health = 4
+		is_dead = false
+
 func anim_play():
 	var mouse_movement = get_global_mouse_position()
 	pos = global_position
@@ -39,9 +63,12 @@ func anim_play():
 			
 	if input_movement != Vector2.ZERO:
 		$Anim.play("Move")
-	
+	 
 	if input_movement == Vector2.ZERO:
 		$Anim.play("Idle")
+		
+	if Input.is_action_just_pressed("ui_shoot"):
+		instance_bullet()
 		
 func target_mouse():
 	var mouse_movement = get_global_mouse_position()
@@ -52,3 +79,11 @@ func target_mouse():
 		gun_sprite.flip_v = false
 	else:
 		gun_sprite.flip_v = true
+
+
+func instance_bullet():
+	var bullet = bullet_scene.instantiate()
+	bullet.direction = bullet_point.global_position - gun.global_position
+	print(bullet.direction)
+	bullet.global_position = bullet_point.global_position
+	get_tree().root.add_child(bullet)
